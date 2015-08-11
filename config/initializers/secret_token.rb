54deab451bd1a8ec7541879885140cea6a6e0ef4
Jean-Julien Fleck <jeanjulien.fleck@gmail.com>
2015-08-11 18:25:45 +0200

    Premier commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GestionCollesR3::Application.config.secret_token = '446e80b2acd726c2b4390083e582242c325055a1afd1d2729f4cddc825d4d97b3de55ba4d51cd0f98338c0eaacfe220bb83084e6188df7b0eb1e209ba7cbc032'
